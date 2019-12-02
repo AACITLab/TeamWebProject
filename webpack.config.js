5f55b0f5a50e4edea707f9cffd7272618fb24de6
AACITLab <45234472+AACITLab@users.noreply.github.com>
2019-12-02 15:48:25 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-// webpack.config.js
+
 module.exports = {
-  // This is the "main" file which should include all other modules
+ 
   entry: './src/main.js',
-  // Where should the compiled file go?
+ 
   output: {
-    
     filename: 'bundle.js'
   },
   resolve: {
@@ -13,22 +12,16 @@ module.exports = {
   }
 },
   module: {
-    // Special compilation rules
     loaders: [
       {
-        // Ask webpack to check: If this file ends with .js, then apply some transforms
         test: /\.js$/,
-        // Transform it with babel
         loader: 'babel-loader',
-        // don't transform node_modules folder (which don't need to be compiled)
         exclude: /node_modules/
       },
       {
-        // Ask webpack to check: If this file ends with .vue, then apply some transforms
         test: /\.vue$/,
-        // don't transform node_modules folder (which don't need to be compiled)
         exclude: /(node_modules|bower_components)/,
-        // Transform it with vue
+        
       use: {
         loader: 'vue-loader'
       }
